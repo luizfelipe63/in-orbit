76f0466c6f420e6e27fc89500e68587aa5d21752
Luiz Felipe <75274860+luizfelipe63@users.noreply.github.com>
2024-09-11 16:32:52 -0300

    lint modifications

--- server/src/http/controllers/create-goals.ts
+++ server/src/http/controllers/create-goals.ts
@@ -1,17 +1,22 @@
-import { FastifyReply, FastifyRequest } from "fastify";
+import type { FastifyReply, FastifyRequest } from "fastify";
 import z from "zod";
 import { createGoal } from "../../use-cases/create-goals";
 
-export async function CreateGoals(request: FastifyRequest, reply: FastifyReply){
-    const createGoalsBodySchema = z.object({
-        title: z.string(),
-        desiredWeeklyFrequency: z.number().int().min(1).max(7),
-      })
-    
-      const { desiredWeeklyFrequency, title} = createGoalsBodySchema.parse(request.body)    
+export async function CreateGoals(
+	request: FastifyRequest,
+	reply: FastifyReply,
+) {
+	const createGoalsBodySchema = z.object({
+		title: z.string(),
+		desiredWeeklyFrequency: z.number().int().min(1).max(7),
+	});
 
-      await createGoal({
-        desiredWeeklyFrequency,
-        title
-      })
+	const { desiredWeeklyFrequency, title } = createGoalsBodySchema.parse(
+		request.body,
+	);
+
+	await createGoal({
+		desiredWeeklyFrequency,
+		title,
+	});
 }
--- server/src/http/controllers/routes.ts
+++ server/src/http/controllers/routes.ts
@@ -1,6 +1,10 @@
-import { FastifyInstance } from "fastify";
+import type { FastifyInstance } from "fastify";
 import { CreateGoals } from "./create-goals";
+import { getPendingGoals } from "./get-pending-goals";
+import { CreateCompletion } from "./create-completion";
 
-export async function routes(app: FastifyInstance){
-    app.post('/goals', CreateGoals)
+export async function routes(app: FastifyInstance) {
+	app.post("/goals", CreateGoals);
+	app.get("/pending-goals", getPendingGoals);
+	app.post("/completion", CreateCompletion);
 }
